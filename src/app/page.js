f3debc586c0f424760300f54dd0c7fbc2ff88fe9
MateoGaspar0304 <fernandezromeromateo@gmail.com>
2024-12-30 09:44:32 -0300

    Menu Principal, y sus estilos css

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <section className="home">
-        <div className="presentation">
-            <img src="LOGO (2).png" alt="Logo de la empresa" className="logo-presentation animated-logo"/>
-            <h1>¡Bienvenidos a Nuestra Web!</h1>
-            <p>Descubrí todas las opciones que tenemos para vos.</p>
-            <Link href="/menu">
-              <button>Explorar</button>
-            </Link>
-        </div>
+      <div className="presentation">
+        <img
+          src="img/LOGO (2).png"
+          alt="Logo de la empresa"
+          className="logo-presentation animated-logo"
+        />
+        <h1>¡Bienvenidos a Nuestra Web!</h1>
+        <p>Descubrí todas las opciones que tenemos para vos.</p>
+        <Link href="/menu">
+          <button>Explorar</button>
+        </Link>
+      </div>
     </section>
   );
 }
