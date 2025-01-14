8929eaabbc06ec22d4e73e8612d799674c9030e5
lautaroperesin <lautaroperesin@gmail.com>
2025-01-14 12:23:48 -0300

    Centrando items de la grilla de productos

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -153,7 +153,7 @@ export default function Menu() {
         <CategoriesNavBar />
 
         {/* Productos */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
           {productosFiltrados.map((product) => (
               <ProductCard key={product.id} product={product} onAddToCart={agregarAlCarrito} />
           ))}
