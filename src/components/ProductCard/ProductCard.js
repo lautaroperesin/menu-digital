52cdb77107a920f6b670739610f72aedd2357427
lautaroperesin <lautaroperesin@gmail.com>
2025-02-03 10:43:18 -0300

    Arreglo de error en el componente ProductCard

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,24 +1,24 @@
 import './ProductCard.css'; 
 
-export default function ProductCard({ product, onAddToCart }) {
+export default function ProductCard({ producto, onAddToCart }) {
   return (
     <div className="product-card-container">
     <div>
       <img
-        src={product.imagen}
-        alt={product.nombre}
+        src={producto.imagen}
+        alt={producto.nombre}
         className="product-image"
       />
     </div>
     <div>
-      <h3 className="product-title">{product.nombre}</h3>
-      <p className="product-description">{product.descripcion}</p>
+      <h3 className="product-title">{producto.nombre}</h3>
+      <p className="product-description">{producto.descripcion}</p>
     </div>
     <div>
-      <p className="product-price">${product.precio}</p>
+      <p className="product-price">${producto.precio}</p>
     </div>
     <div>
-      <button onClick={() => onAddToCart(product)}>
+      <button onClick={() => onAddToCart(producto)}>
         AGREGAR
       </button>
     </div>
