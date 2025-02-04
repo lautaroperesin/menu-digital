ea45d3ad0fbaf9bea20ac7da60495ce08597efe1
MateoGaspar0304 <fernandezromeromateo@gmail.com>
2025-02-04 11:22:38 -0300

    Menu (menu), estilo, fondo y diseño nuevo.

--- src/app/(sections)/menu/page.js
+++ src/app/(sections)/menu/page.js
@@ -1,33 +1,36 @@
 'use client';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import styles from './Menu.module.css'; 
 
 export default function Menu() {
   const [categorias, setCategorias] = useState([]);
 
   useEffect(() => {
     const fetchCategorias = async () => {
-      const response = await fetch('/api/categorias');
-      const data = await response.json();
-      setCategorias(data);
+      try {
+        const response = await fetch('/api/categorias');
+        if (!response.ok) {
+          throw new Error('Error al cargar las categorías');
+        }
+        const data = await response.json();
+        setCategorias(data);
+      } catch (error) {
+        console.error(error);
+      }
     };
     fetchCategorias();
   }, []);
 
   return (
-    <>
-      <div className='home'>
-        <div className='presentation'>
+    <div className={styles.home}>
+      <div className={styles.presentation}>
         {categorias.map((categoria) => (
-        <Link href={`/menu/${categoria.id}`} key={categoria.id}>
-          <button>
-          {categoria.nombre}
-          </button>
-        </Link>
+          <Link href={`/menu/${categoria.id}`} key={categoria.id}>
+            <button className={styles.button}>{categoria.nombre}</button>
+          </Link>
         ))}
-        </div>
-
       </div>
-    </>
+    </div>
   );
 }
